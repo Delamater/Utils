90263f4e26a3c6cb01ee13ae4d361364055803d2
Bob Delamater <bob.delamater@sage.com>
2015-01-21 14:14:10 -0800

    Update CreateSoftSymbolicLink.bat

--- CreateSoftSymbolicLink.bat
+++ CreateSoftSymbolicLink.bat
@@ -1,8 +1,15 @@
 @ECHO OFF
 
-SET myPhysicalPath="E:\MyNewX3Folder\PRODUCTION"
-SET myVirtualPath="C:\SAGE\SAGEX3V6\X3V6\Folders\PRODUCTION"
+SET myPhysicalPath="E:\MyNewX3Folder\ECCPROD"
+SET myVirtualPath="C:\SAGE\SAGEX3V6\X3V6\Folders\ECCPROD"
 mklink /D %myVirtualPath% %myPhysicalPath% 
 
 ECHO mklink /D %myPhysicalPath% %myVirtualPath%
+
+
+SET myPhysicalPath="E:\MyNewX3Folder\X3PUB\ECCPROD"
+SET myVirtualPath="C:\SAGE\SAGEX3V6\X3V6\Folders\X3_PUB\ECCPROD"
+mklink /D %myVirtualPath% %myPhysicalPath% 
+ECHO mklink /D %myPhysicalPath% %myVirtualPath%
+
 pause
